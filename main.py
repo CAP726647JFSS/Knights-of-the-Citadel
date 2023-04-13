cb51597435ed760bd5b7a561e2acfe2cebab451c
ChrisPop <130321899+CAP726647JFSS@users.noreply.github.com>
2023-04-13 13:19:28 +0000

    Added tower, fixed projectiles, modified projectile firing system and speed, modified assets file structure.

--- main.py
+++ main.py
@@ -14,17 +14,22 @@ circleX = 600
 circleY = 600
 projectiles = []
 
+castle_image = pygame.transform.scale(pygame.image.load('Assets/Tower/tower.png'), (85, 130))
+basic_bullet_image = pygame.transform.scale(pygame.image.load('Assets/Bullets/light_bullet.png'), (10, 10))
+
+
 runGame = True
 while runGame:
 
   window.fill((0, 0, 0))
   window.blit(background_image, (0,0))
+  window.blit(castle_image, (385,168))
   
   mouseX, mouseY = pygame.mouse.get_pos()
   try:
-    mouseAngle = math.atan2((mouseY - 250), (mouseX - 450))
+    mouseAngle = math.atan2((mouseY - 190), (mouseX - 430))
   except ZeroDivisionError:
-    mouseAngle = math.atan2((mouseY - 250), (mouseX - 449))
+    mouseAngle = math.atan2((mouseY - 190), (mouseX - 429))
   
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
@@ -32,10 +37,10 @@ while runGame:
 
     # fire the projectile when the spacebar is pressed
     if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        pos = [450, 250]
-        vel = [math.cos(mouseAngle), math.sin(mouseAngle)]
+        pos = [430 + math.cos(mouseAngle) * 25, 190 + math.sin(mouseAngle) * 25]
+        vel = [math.cos(mouseAngle) * 20, math.sin(mouseAngle) * 20]
         size = 10
-        projectile = Projectile(pos, vel, size)
+        projectile = Projectile(pos, vel, size, basic_bullet_image)
         projectiles.append(projectile)
 
   # update the projectiles
--- projectile.py
+++ projectile.py
@@ -2,11 +2,12 @@ import pygame
 import math
 
 class Projectile:
-    def __init__(self, pos, vel, size, sprite):
+    def __init__(self, pos, vel, size, sprite, sprite_frames = []):
         self.pos = pos
         self.vel = vel
         self.size = size
         self.angle = 0
+        self.sprite_frames = [sprite]
         self.sprite = sprite
         self.frame = 0
         self.animation_speed = 5  # Number of frames to wait before switching to next animation frame
@@ -19,11 +20,12 @@ class Projectile:
         self.frame_counter += 1
         if self.frame_counter >= self.animation_speed:
             self.frame_counter = 0
-            self.frame = (self.frame + 1) % len(self.sprite)
+            self.frame = (self.frame + 1) % len(self.sprite_frames)
+            self.sprite = self.sprite_frames[self.frame]
 
     def draw(self, surface):
         rotated_surface = pygame.transform.rotate(pygame.Surface((self.size*2, self.size*2), pygame.SRCALPHA), -self.angle)
-        sprite_rect = pygame.Rect(self.sprite[self.frame].get_rect())
+        sprite_rect = pygame.Rect(self.sprite.get_rect())
         sprite_rect.center = (self.size, self.size)
-        rotated_surface.blit(self.sprite[self.frame], sprite_rect)
+        rotated_surface.blit(self.sprite, sprite_rect)
         surface.blit(rotated_surface, (self.pos[0]-self.size, self.pos[1]-self.size))
